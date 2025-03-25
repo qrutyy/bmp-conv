04647d38c98a5a62f403c300a58e4f6173c8f424
qrutyy <qrutyq@gmail.com>
2025-03-25 12:43:02 +0300

    test: update verification tests

--- tests/test.sh
+++ tests/test.sh
@@ -107,27 +107,23 @@ if [ "$VERIFY" == "false" ]; then
 	python3 "$SD/comp-plots.py"
 
 else 
-	echo -e "\nRunning single-threaded tests"
+	echo -e "\nRunning single-threaded verification tests"
 	for fil in "${FILTERS[@]}"; do
-		make -C "$BD" run-p-cores INPUT_TF="$TEST_FILE" FILTER_TYPE="$fil" THREAD_NUM="$THREADNUM" LOG=0
+		make -C "$BD" run-p-cores INPUT_TF="$TEST_FILE" FILTER_TYPE="$fil" THREAD_NUM="$THREADNUM" LOG=0 > /dev/null
 
-		for i in $(seq 1 "$RUN_NUM"); do
-			make -C "$BD" run-p-cores INPUT_TF="$TEST_FILE" FILTER_TYPE="$fil" THREAD_NUM=1 LOG=0 
+			make -C "$BD" run-p-cores INPUT_TF="$TEST_FILE" FILTER_TYPE="$fil" THREAD_NUM=1 LOG=0 > /dev/null 
 			compare_results "$TEST_FILE"
-		done
 	done
 
 
-	echo -e "\nRunning multithreaded tests"
+	echo -e "\nRunning multithreaded verification tests"
 	for mode in "${MODES[@]}"; do
 		for fil in "${FILTERS[@]}"; do
 			for bs in "${BLOCK_SIZE[@]}"; do
-				make -C "$BD" run-p-cores INPUT_TF="$TEST_FILE" FILTER_TYPE="$fil" THREAD_NUM=1 LOG=0
+				make -C "$BD" run-p-cores INPUT_TF="$TEST_FILE" FILTER_TYPE="$fil" THREAD_NUM=1 LOG=0 >/dev/null
 
-				for i in $(seq 1 "$RUN_NUM"); do
-					make -C "$BD" run-p-cores INPUT_TF="$TEST_FILE" FILTER_TYPE="$fil" THREAD_NUM="$THREADNUM" BLOCK_SIZE="$bs" COMPUTE_MODE="$mode" LOG=0
+					make -C "$BD" run-p-cores INPUT_TF="$TEST_FILE" FILTER_TYPE="$fil" THREAD_NUM="$THREADNUM" BLOCK_SIZE="$bs" COMPUTE_MODE="$mode" LOG=0 > /dev/null
 					compare_results "$TEST_FILE"
-				done
 			done
 		done
 	done
