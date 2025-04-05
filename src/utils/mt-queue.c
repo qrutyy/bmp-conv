f128be08b895e82e2c74039dc668617ebd0cf533
qrutyy <qrutyq@gmail.com>
2025-04-05 13:33:27 +0300

    fix: correct mt threads sync

--- src/utils/mt-queue.c
+++ src/utils/mt-queue.c
@@ -2,8 +2,12 @@
 
 #include "mt-queue.h"
 #include "mt-utils.h"
+#include "utils.h"
 #include <pthread.h>
 #include <string.h>
+#include <stdatomic.h>
+#include <sys/time.h>
+#include <errno.h>
 
 // TODO: add better logging + test
 
@@ -15,6 +19,14 @@
 */
 
 #define RAW_MEM_OVERHEAD (512 * 1024) // some auxiliary data structures and all this stuff...
+#define NSEC_OFFSET (1000 * 1000000) // 100 ms in nanoseconds
+
+// due to queue_pop sleep mechanism - we should use global atomic for writers
+size_t written_files = 0;
+// almost the same with read_files
+size_t read_files = 0;
+
+pthread_barrier_t reader_barrier;
 
 // yeah, its not that precise. baytik tuda syuda...
 static size_t estimate_image_memory(const bmp_img *img)
@@ -49,7 +61,11 @@ static void queue_push(struct img_queue *q, bmp_img *img, char *filename)
 {
 	struct queue_img_info *iq_info = NULL;
 	size_t image_memory = 0;
+	double start_block_time = 0;
+	double result_time = 0;
 
+	if (!filename)
+		return;
 	iq_info = malloc(sizeof(struct queue_img_info));
 	if (!iq_info) {
 		fprintf(stderr, "Error: memory allocation failed in 'queue_push'\n");
@@ -62,32 +78,72 @@ static void queue_push(struct img_queue *q, bmp_img *img, char *filename)
 
 	image_memory = estimate_image_memory(img);
 
-	while (q->size == MAX_QUEUE_SIZE || (q->current_mem_usage + image_memory > q->max_mem_usage && q->size > 0)) {
-		pthread_cond_wait(
-			&q->cond_non_full,
-			&q->mutex); // unblocks threads with other roles (f.e. workers, that require some elements for convolution) and waits for return cond_non_full signal
+	while (q->size >= MAX_QUEUE_SIZE || (q->current_mem_usage + image_memory > q->max_mem_usage && q->size > 0)) {
+		start_block_time = get_time_in_seconds();
+		pthread_cond_wait(&q->cond_non_full,&q->mutex); 
+		// unblocks threads with other roles (f.e. workers, that require some elements for convolution) 
+		// and waits for return cond_non_full signal
 	}
+
+	result_time = (start_block_time != 0) ? get_time_in_seconds() - start_block_time : 0;
 
 	q->images[q->rear] = iq_info;
 	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
 	q->size++;
 	q->current_mem_usage += image_memory;
 
+	if (result_time)
+		qt_write_logs(result_time, QPUSH);
+
 	pthread_cond_signal(&q->cond_non_empty);
 	pthread_mutex_unlock(&q->mutex);
 }
 
-static bmp_img *queue_pop(struct img_queue *q, char **filename)
+static bmp_img *queue_pop(struct img_queue *q, char **filename, uint8_t file_count)
 {
 	struct queue_img_info *iqi = NULL;
 	bmp_img *img_src = NULL;
 	size_t image_memory = 0;
+	double start_block_time = 0;
+	double result_time = 0;
+	struct timespec wait_time;
+	int wait_result = 0;
 
 	pthread_mutex_lock(&q->mutex);
 
-	while (q->size == 0) // waiting for some images to pop from queue
-		pthread_cond_wait(&q->cond_non_empty, &q->mutex);
+restart_loop:
 
+	while (q->size == 0) {// waiting for some images to pop from queue
+		start_block_time = get_time_in_seconds();
+		if (__atomic_load_n(&written_files, __ATOMIC_ACQUIRE) >= file_count)
+			return NULL;
+		
+		clock_gettime(CLOCK_REALTIME, &wait_time);
+        wait_time.tv_nsec += NSEC_OFFSET;
+        wait_time.tv_sec += wait_time.tv_nsec / 1000000000;
+        wait_time.tv_nsec %= 1000000000;
+		wait_result = pthread_cond_timedwait(&q->cond_non_empty, &q->mutex, &wait_time);
+
+		if (wait_result == ETIMEDOUT) {
+             printf("Consumer timed out.\n");
+            if (__atomic_load_n(&written_files, __ATOMIC_ACQUIRE) >= file_count) {
+                goto exit_overload; 
+			}
+			goto restart_loop;
+        } else if (wait_result == 0) {
+             printf("Consumer signalled.\n");
+            if (__atomic_load_n(&written_files, __ATOMIC_ACQUIRE) >= file_count) {
+                goto exit_overload;
+            }
+	    } else {
+            perror("pthread_cond_timedwait error");
+            pthread_mutex_unlock(&q->mutex);
+            goto exit_overload;
+        }
+	}
+
+	result_time = (start_block_time != 0) ? get_time_in_seconds() - start_block_time : 0;
+	
 	iqi = q->images[q->front];
 	image_memory = estimate_image_memory(iqi->image);
 
@@ -97,9 +153,8 @@ static bmp_img *queue_pop(struct img_queue *q, char **filename)
 
 	if (iqi->filename) {
 		// NOLINTBEGIN
-		*filename = strdup(iqi->filename);
+		*filename = strdup(iqi->filename); // strdup = malloc + strcpy
 		// NOLINTEND
-		// strdup = malloc + strcpy
 		if (!*filename) {
 			fprintf(stderr, "Error: strdup failed in queue_pop\n");
 		}
@@ -108,9 +163,17 @@ static bmp_img *queue_pop(struct img_queue *q, char **filename)
 	img_src = iqi->image;
 	free(iqi);
 
+	if (result_time)
+		qt_write_logs(result_time, QPOP);
+
 	pthread_cond_signal(&q->cond_non_full);
 	pthread_mutex_unlock(&q->mutex);
 	return img_src;
+
+exit_overload:
+	pthread_cond_signal(&q->cond_non_full);
+	pthread_mutex_unlock(&q->mutex);
+	return NULL;
 }
 
 // just reads the image from the queue. doesn't set up img-specific structures
@@ -121,29 +184,48 @@ void *reader_thread(void *arg)
 	bmp_img *img;
 	char filepath[MAX_PATH_LEN];
 	bmp_img *empty_img = NULL;
+	double start_time = 0;
+	double result_time = 0;
+	size_t read_files_local = 0;
+	
+	while (1) {
+		read_files_local = __atomic_fetch_add(&read_files, 1, __ATOMIC_ACQUIRE);
+		if (read_files_local >= qt_info->pargs->file_count)
+			break;
 
-	for (i = 0; i < qt_info->pargs->file_count; i++) {
+		start_time = get_time_in_seconds();
+
 		img = malloc(sizeof(bmp_img));
 		if (!img) {
 			fprintf(stderr, "Reader Error: Failed to allocate bmp_img struct\n");
-			continue;
+			break;
 		}
-		snprintf(filepath, sizeof(filepath), "test-img/%s", qt_info->pargs->input_filename[i]);
+		snprintf(filepath, sizeof(filepath), "test-img/%s", qt_info->pargs->input_filename[read_files_local]);
 		if (bmp_img_read(img, filepath)) {
-			fprintf(stderr, "Reader Error: Could not open %s\n", qt_info->pargs->input_filename[i]);
+			fprintf(stderr, "Reader Error: Could not open %s\n", qt_info->pargs->input_filename[read_files_local]);
 			exit(-1); // panic button
 		}
-		queue_push(qt_info->input_q, img, qt_info->pargs->input_filename[i]);
+		queue_push(qt_info->input_q, img, qt_info->pargs->input_filename[read_files_local]);
+
+		// future work(termination signals) is made more for integrity and better threads work,
+		// so ending time measuring here. 
+		result_time = get_time_in_seconds() - start_time;
+		if (result_time)
+			qt_write_logs(result_time, READER);
+		
 		printf("Reader: Read %s\n", filepath);
 	}
-	for (i = 0; i < qt_info->pargs->wot_count; i++) {
+	
+	pthread_barrier_wait(&reader_barrier); // waiting for pushing every file to queue (to avoid early termination signal)
+
+	for (i = 0; i < (size_t)(qt_info->pargs->wot_count / qt_info->pargs->ret_count + 2); i++) {
 		empty_img = calloc(1, sizeof(bmp_img));
 		if (empty_img) {
-			queue_push(qt_info->input_q, empty_img, NULL); // Передаем указатель на пустую структуру
+			queue_push(qt_info->input_q, empty_img, NULL);
 		} else {
 			fprintf(stderr, "Reader Error: Failed to allocate termination signal\n");
 		}
-		queue_push(qt_info->input_q, empty_img, "");
+		queue_push(qt_info->input_q, empty_img, NULL);
 	}
 	printf("Reader: Finished reading files and sent termination signals.\n");
 	return NULL;
@@ -152,8 +234,6 @@ void *reader_thread(void *arg)
 void *worker_thread(void *arg)
 {
 	struct qthreads_info *qt_info = (struct qthreads_info *)arg;
-	printf("Worker started.\n");
-
 	uint16_t next_x_block_local, next_y_block_local, process_result = 0;
 	pthread_mutex_t local_xy_mutex = PTHREAD_MUTEX_INITIALIZER;
 	bmp_img *img = NULL;
@@ -162,16 +242,21 @@ void *worker_thread(void *arg)
 	struct img_dim *dim = NULL;
 	struct img_spec *img_spec = NULL;
 	char *filename = NULL;
+	double start_time = 0;
+	double result_time = 0;
 
 	while (1) {
-		img = queue_pop(qt_info->input_q, &filename);
+		// even though allocation are time consuming - for comparison in this case we need raw thread time
+		start_time = get_time_in_seconds();
+
+		img = queue_pop(qt_info->input_q, &filename, qt_info->pargs->file_count);
 		if (!img) {
+			// determines signal receivement.
 			fprintf(stderr, "Worker Error: queue_pop returned NULL image pointer\n");
 			if (filename)
-				free(filename); // Освобождаем имя файла, если оно было получено
-			continue;
+				free(filename);
+			break;
 		}
-		printf("height %d, width %d \n", img->img_header.biHeight, img->img_header.biWidth);
 
 		if (img->img_header.biWidth == 0 && img->img_header.biHeight == 0) {
 			printf("Worker: Received termination signal.\n");
@@ -249,6 +334,11 @@ void *worker_thread(void *arg)
 			filter_part_computation(th_spec, qt_info->pargs->filter_type, qt_info->filters);
 		}
 		queue_push(qt_info->output_q, th_spec->img->output_img, filename);
+		
+		result_time = get_time_in_seconds() - start_time;
+		if (result_time)
+			qt_write_logs(result_time, WORKER);
+
 		printf("Worker Pushed result to output queue.\n");
 
 		bmp_img_free(img);
@@ -266,29 +356,43 @@ void *writer_thread(void *arg)
 	struct qthreads_info *qt_info = (struct qthreads_info *)arg;
 	char output_filepath[MAX_PATH_LEN];
 	bmp_img *img = NULL;
-	size_t i = 0;
 	char *filename = malloc(sizeof(char));
+	double start_time = 0;
+	double result_time = 0;
+	ssize_t current_wf = 0;
 
 	printf("File count: %d\n", qt_info->pargs->file_count);
 
-	for (i = 0; i < qt_info->pargs->file_count; i++) {
-		img = queue_pop(qt_info->output_q, &filename);
+	while (1) {
+		start_time = get_time_in_seconds();
+
+		if (__atomic_load_n(&written_files, __ATOMIC_ACQUIRE) >= qt_info->pargs->file_count)
+			break;
+		
+		img = queue_pop(qt_info->output_q, &filename, qt_info->pargs->file_count);
+		if (!img)
+			break;
+
 		if (img->img_header.biWidth == 0 && img->img_header.biHeight == 0) {
 			break;
 		}
-		printf("%s\n", qt_info->pargs->output_filename);
+
 		if (strlen(qt_info->pargs->output_filename))
 			snprintf(output_filepath, sizeof(output_filepath), "test-img/q_out_%s_%s", qt_info->pargs->output_filename, filename);
 		else
 			snprintf(output_filepath, sizeof(output_filepath), "test-img/q_out_%s", filename);
 
-		bmp_img_write(img, output_filepath); // getting input image at output
+		bmp_img_write(img, output_filepath);
+		
+		result_time = get_time_in_seconds() - start_time;
+		if (result_time)
+			qt_write_logs(result_time, WRITER);
+
 		bmp_img_free(img);
-
-		printf("Successfully write %s\n", output_filepath);
-
-		free(img);
+		current_wf = __atomic_add_fetch(&written_files, 1, __ATOMIC_ACQUIRE);
+		printf("Successfully write %s file with index %zu\n", output_filepath, current_wf);
 	}
+	pthread_cond_signal(&qt_info->output_q->cond_non_empty); // sending signal in case some writer thread is waiting in queue_pop
 	return NULL;
 }
 
@@ -342,6 +446,9 @@ void create_qthreads(struct qthreads_info *qt_info, struct p_args *args_ptr)
 {
 	size_t i;
 	printf("Creating %hhu readers, %hhu workers, %hhu writers\n", args_ptr->ret_count, args_ptr->wot_count, args_ptr->wrt_count);
+
+	pthread_barrier_init(&reader_barrier, NULL, qt_info->pargs->ret_count);
+
 	for (i = 0; i < args_ptr->ret_count; i++) {
 		if (pthread_create(&qt_info->ret_info->threads[i], NULL, reader_thread, qt_info)) {
 			perror("Failed to create a reader thread");
@@ -370,24 +477,24 @@ void create_qthreads(struct qthreads_info *qt_info, struct p_args *args_ptr)
 void join_qthreads(struct qthreads_info *qt_info)
 {
 	size_t i;
-
-	for (i = 0; i < qt_info->wrt_info->used_threads; i++) {
-		if (pthread_join(qt_info->wrt_info->threads[i], NULL)) {
-			perror("Failed to join a writer thread");
+	for (i = 0; i < qt_info->ret_info->used_threads; i++) {
+		if (pthread_join(qt_info->ret_info->threads[i], NULL)) {
+			fprintf(stderr, "Failed to join a reader thread");
 		}
 	}
 
 	for (i = 0; i < qt_info->wot_info->used_threads; i++) {
 		if (pthread_join(qt_info->wot_info->threads[i], NULL)) {
-			perror("Failed to join a worker thread");
+			fprintf(stderr, "Failed to join a worker thread");
 		}
 	}
 
-	for (i = 0; i < qt_info->ret_info->used_threads; i++) {
-		if (pthread_join(qt_info->ret_info->threads[i], NULL)) {
-			perror("Failed to join a reader thread");
+	for (i = 0; i < qt_info->wrt_info->used_threads; i++) {
+		if (pthread_join(qt_info->wrt_info->threads[i], NULL)) {
+			fprintf(stderr, "Failed to join a writer thread");
 		}
 	}
+	pthread_barrier_destroy(&reader_barrier);
 }
 
 void free_qthread_resources(struct qthreads_info *qt_info)
